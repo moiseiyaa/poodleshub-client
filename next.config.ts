0fa5ef3a3e1e91d496e0a4a210d8db49e04ec07a
moiseiyaa <moiseiyaa@gmail.com>
2026-01-16 20:50:08 +0200

    updated next js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // Allow Cloudinary-hosted images (e.g. res.cloudinary.com/<cloud_name>/ ...)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
